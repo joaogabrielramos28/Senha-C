0c53d6ed1e7354fc3fa1b49e2616de0ffd284110
vinipavani <viniciusalvespavani@hotmail.com>
2021-02-23 22:47:03 -0300

    + logica de entrada, + validacao da entrada do jogador

--- senha.c
+++ senha.c
@@ -2,13 +2,26 @@
 #include <stdlib.h>
 #include <time.h>
 #define TAM 4
+
 int main(void){
-    int senha[TAM],i;
+    int senha[TAM],i, senha_inserida[TAM], j;
     srand(time(NULL));
     for(i=0;i<TAM;i++){
         senha[i]=rand() % 7;
-        
-    }printf("senha:****\n");
-
-    return 1;
+    }
+    printf("senha:****\n");
+    
+    /*Recebe a tentativa do usuario*/
+    scanf("%d%d%d%d", &senha_inserida[0], &senha_inserida[1], &senha_inserida[2], &senha_inserida[3]);
+    for (j=0; j<TAM; j++){
+        if (senha_inserida[j] == senha[j]){
+            /*Verificar se o caracter está na senha e na posição correta*/
+        } else if ((senha_inserida[j] == senha[0]) || 
+        (senha_inserida[j] == senha[1]) || 
+        (senha_inserida[j] == senha[2]) || 
+        (senha_inserida[j] == senha[3]) ){
+            /*Verifica se o caracter está na senha, porém na posição incorreta*/
+        }
+    }
+    return 0;
 }
