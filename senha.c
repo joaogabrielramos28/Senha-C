6b8f2720ae1c4449f911fbc91f2d872ece032e5d
vinipavani <viniciusalvespavani@hotmail.com>
2021-02-25 01:36:05 -0300

    + Ciclo de jogo. *senha gera algarismo repetido
    Explicando melhor o que houve, trabalhei mais em algumas condicionais e criei o loop pro jogo, só que havia um problema sempre que a senha gerada continha o valor '0' o computador lia como vazio ent considerava uma senha de 3 caracteres pelo menos foi o que eu entendi. Por isso criei essa branch optando pelo retorno dos inteiros mas ainda estou lidando com outros problemas como senha estar gerando algarismos repetidos.

--- senha.c
+++ senha.c
@@ -8,64 +8,79 @@
 
 int main(void)
 {
-        
-        char senha[TAM],senha_inserida[TAM];
-        int i, j, tentativa, flag = 0;
-        srand((unsigned)time(NULL));
+    int senha[TAM],senha_inserida[TAM];
+    int i, j, k, tentativa, flag, pos_certa, pos_errada = 0;
+    srand((unsigned)time(NULL));
 
-        /* Zerando vetores */
-        senha[0] = 0;
-        senha_inserida[0] = 0;
-        senha[1] = 0;
-        senha_inserida[1] = 0;
-        senha[2] = 0;
-        senha_inserida[2] = 0;
-        senha[3] = 0;
-        senha_inserida[3] = 0;
+    /* Zerando vetores */
+    senha[0] = 0;
+    senha_inserida[0] = 0;
+    senha[1] = 0;
+    senha_inserida[1] = 0;
+    senha[2] = 0;
+    senha_inserida[2] = 0;
+    senha[3] = 0;
+    senha_inserida[3] = 0;
 
-        /*Gerador de senha*/
-        for (i = 0; i < 4; i++)
+    /*Gerador de senha*/
+    for (i = 0; i < 4; i++)
+    {
+        srand((unsigned int)time(NULL));
+        senha[i] = (rand() % (7));
+        for (j = 0; senha[j] < 4; j++)
         {
-            srand((unsigned int)time(NULL));
-            senha[i] = (rand() % (7)) + 48;
-
-            for (j = 0; senha[j] != '\0'; j++)
+            if ((senha[j] == senha[i]) && (i != j))
             {
-                if ((senha[j] == senha[i]) && (i != j) && (flag == 0))
-                {
-                    i--;
-                    break;
-                }
+                i--;
+                break;
             }
         }
-        printf("%s\n", senha);
+    }
+    printf("%d%d%d%d\n", senha[0], senha[1], senha[2], senha[3]);
     printf("senha:****\n");
 
-/*Recebe a tentativa do usuario*/
-    scanf("%s\n",senha_inserida);
-    for (j = 0; j < TAM; j++)
-    {
-        
-
-
-        if (senha_inserida[j] == senha[j])
+    tentativa = 0;
+    pos_certa = 0;
+    pos_errada = 0;
+    /*Ciclo de jogo em 15 tentativas*/
+    while(tentativa < 16)
+    {   
+        printf("Voce tem %d tentativas\n", (15-tentativa));
+        printf("Digite sua tentativa: \n");
+        /*Recebe a tentativa do usuario*/
+        scanf("%d%d%d%d\n", &senha_inserida[0], &senha_inserida[1], &senha_inserida[2], &senha_inserida[3]);
+        for (k = 0; k < TAM; k++)
         {
-            
             /*Verificar se o caracter está na senha e na posição correta*/
-        }
-        else if ((senha_inserida[j] == senha[0]) ||
-                (senha_inserida[j] == senha[1]) ||
-                (senha_inserida[j] == senha[2]) ||
-                (senha_inserida[j] == senha[3]))
-        {
-            printf("%s esta na posicao correta \n",senha_inserida[j]);
-        }
-
-           
-        
+            if (senha_inserida[k] == senha[k])
+            {  
+                printf("%d esta na posicao correta\n",senha_inserida[k]);
+                pos_certa += 1;
+            }
             /*Verifica se o caracter está na senha, porém na posição incorreta*/
-        
-        return 0;
-    }
-
+            else if((senha_inserida[k] == senha[0]) ||
+                    (senha_inserida[k] == senha[1]) ||
+                    (senha_inserida[k] == senha[2]) ||
+                    (senha_inserida[k] == senha[3]))
+            {
+                printf("%d esta na posicao incorreta, mas esta na senha\n",senha_inserida[k]);
+                pos_errada += 1;
+            }
+            /*Caso onde o usuário não acertou nenhum caracter da senha, mesmo fora de ordem*/
+            else if((k == 3) && (pos_certa == 0) && (pos_errada == 0))
+            {
+                printf("Nenhum dos caracteres inseridos esta na senha\n");
+            }
+            /*Caso onde todos os caracteres foram inseridos na posição correta*/
+            if (pos_certa == 4)
+            {
+                printf("Parabens voce acertou a senha!");
+                return 1;
+            }
+        } 
+        pos_certa = 0;
+        pos_errada = 0;
+        tentativa++;
+    } 
+    return 0;
 }
