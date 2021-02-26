d0d0c7b64d58d0bc029da3e25a19f3c8e2f1a0b8
joaogabrielramos28 <joaogabrielramos28@outlook.com>
2021-02-25 22:11:33 -0300

    Fiscalização se estao não posição correta

--- senha.c
+++ senha.c
@@ -4,83 +4,144 @@
 #include <time.h>
 #include <string.h>
 #include <ctype.h>
+
 #define TAM 4
 
-int main(void)
-{
-    int senha[TAM],senha_inserida[TAM];
-    int i, j, k, tentativa, flag, pos_certa, pos_errada = 0;
-    srand((unsigned)time(NULL));
-
-    /* Zerando vetores */
+int main(){
+    char senha[TAM],senha_inserida[TAM];
+    int i,j,tentativa;
+    
     senha[0] = 0;
-    senha_inserida[0] = 0;
     senha[1] = 0;
-    senha_inserida[1] = 0;
     senha[2] = 0;
-    senha_inserida[2] = 0;
     senha[3] = 0;
-    senha_inserida[3] = 0;
-
-    /*Gerador de senha*/
-    for (i = 0; i < 4; i++)
-    {
-        srand((unsigned int)time(NULL));
-        senha[i] = (rand() % (7));
-        for (j = 0; senha[j] < 4; j++)
+    senha[4] = '\0';
+    for ( i = 0; i < 4; i++ )
         {
-            if ((senha[j] == senha[i]) && (i != j))
+            srand( (unsigned int) time( NULL ) );
+            senha[i] = ( rand() % (7) ) + 48;
+
+            
+            for ( j = 0; senha[j] !='\0'; j++ )
             {
-                i--;
-                break;
+                if ( ( senha[j] == senha[i] ) && ( i != j ) )
+                {
+                    i--;
+                    break;
+                }
             }
+
+        }printf("%s\n\n",senha);
+        
+        for(tentativa = 1 ; tentativa < 15 ; tentativa++){
+            
+            
+            
+            printf("Tentativa numero %d\n",tentativa);
+            
+            printf("Digite sua tentativa de 4 digitos\n");
+            fflush(stdin);
+            scanf("%s",senha_inserida);
+            
+          
+            
+            
+                
+                  if(senha_inserida[0] == senha[0] && senha_inserida[1] == senha[1] && senha_inserida[2] == senha[2] && senha_inserida[3] == senha[3]){
+                      printf("Ganhou, parabens!!\n");
+                      break;
+                      
+                      
+                  }else if(senha[0] == senha_inserida[0] && senha[1] == senha_inserida[1] && senha[2]==senha_inserida[2]){
+                      printf("%c esta na posicao correta\n",senha_inserida[0]);
+                      printf("%c esta na posicao correta\n",senha_inserida[1]);
+                      printf("%c esta na posicao correta\n",senha_inserida[2]);
+                  }
+                  else if(senha[0] == senha_inserida[0] && senha[1] == senha_inserida[1]){
+                       printf("%c esta na posicao correta\n",senha_inserida[0]);
+                       printf("%c esta na posicao correta\n",senha_inserida[1]);
+                  }
+                  else if(senha[0] == senha_inserida[0] && senha[2] == senha_inserida[2]){
+                       printf("%c esta na posicao correta\n",senha_inserida[2]);
+                       printf("%c esta na posicao correta\n",senha_inserida[0]);
+                  }
+                  else if(senha[0] == senha_inserida[0] && senha[3] == senha_inserida[3]){
+                       printf("%c esta na posicao correta\n",senha_inserida[0]);
+                       printf("%c esta na posicao correta\n",senha_inserida[3]);
+                  }
+                  else if(senha[1] == senha_inserida[1] && senha[3] == senha_inserida[3] && senha[2]==senha_inserida[2]){
+                      printf("%c esta na posicao correta\n",senha_inserida[1]);
+                      printf("%c esta na posicao correta\n",senha_inserida[2]);
+                      printf("%c esta na posicao correta\n",senha_inserida[3]);
+                  }
+                  else if(senha[1] == senha_inserida[1] && senha[2]==senha_inserida[2]){
+                      printf("%c esta na posicao correta\n",senha_inserida[1]);
+                      printf("%c esta na posicao correta\n",senha_inserida[2]);
+                  }
+                  else if(senha[1] == senha_inserida[1] && senha[3] == senha_inserida[3]){
+                      printf("%c esta na posicao correta\n",senha_inserida[1]);
+                      printf("%c esta na posicao correta\n",senha_inserida[3]);
+                  }
+                  else if(senha[0] == senha_inserida[0] && senha[2] == senha_inserida[2] && senha[3] == senha_inserida[3]){
+                    printf("%c esta na posicao correta\n",senha_inserida[0]);
+                    printf("%c esta na posicao correta\n",senha_inserida[2]);
+                    printf("%c esta na posicao correta\n",senha_inserida[3]);
+                  } 
+                  
+                  else if(senha[0] == senha_inserida[0] && senha[1] == senha_inserida[1] && senha[3] == senha_inserida[3]){
+                      printf("%c esta na posicao correta\n",senha_inserida[0]);
+                      printf("%c esta na posicao correta\n",senha_inserida[1]);
+                      printf("%c esta na posicao correta\n",senha_inserida[3]);
+                    }
+                  else if(senha[0] == senha_inserida[0] && senha[2] == senha_inserida[2] && senha[3] == senha_inserida[3]){
+                      printf("%c esta na posicao correta\n",senha_inserida[0]);
+                      printf("%c esta na posicao correta\n",senha_inserida[2]);
+                      printf("%c esta na posicao correta\n",senha_inserida[3]);
+                  }
+                  
+                  else if(senha[3] == senha_inserida[3] && senha[2] == senha_inserida[2]){
+                      printf("%c esta na posicao correta\n",senha_inserida[2]);
+                      printf("%c esta na posicao correta\n",senha_inserida[3]);
+                  }
+                  else if(senha[0] == senha_inserida[0]){
+                      printf("%c esta na posicao correta\n",senha_inserida[0]);
+                  }
+                  else if(senha[1] == senha_inserida[1]){
+                      printf("%c esta na posicao correta\n",senha_inserida[1]);
+                  }
+                  else if(senha[2] == senha_inserida[2]){
+                      printf("%c esta na posicao correta\n",senha_inserida[2]);
+                  }
+                  else if(senha[3] == senha_inserida[3]){
+                      printf("%c esta na posicao correta\n",senha_inserida[3]);
+                  }
+                  
+                  else{
+                      for(j = 0; j < TAM ;j++){
+                          if(senha[j] == senha_inserida[0] ||
+                          senha[j] == senha_inserida[1] ||
+                          senha[j] == senha_inserida[2]||
+                          senha[j] == senha_inserida[3]){
+                              
+                            printf("%c esta na posicao incorreta, mas esta na senha\n",senha[j]);
+                          }
+                      }
+                  }
+                  
+                  
+                  
         }
-    }
-    printf("%d%d%d%d\n", senha[0], senha[1], senha[2], senha[3]);
-    printf("senha:****\n");
-
-    tentativa = 0;
-    pos_certa = 0;
-    pos_errada = 0;
-    /*Ciclo de jogo em 15 tentativas*/
-    while(tentativa < 16)
-    {   
-        printf("Voce tem %d tentativas\n", (15-tentativa));
-        printf("Digite sua tentativa: \n");
-        /*Recebe a tentativa do usuario*/
-        scanf("%d%d%d%d\n", &senha_inserida[0], &senha_inserida[1], &senha_inserida[2], &senha_inserida[3]);
-        for (k = 0; k < TAM; k++)
-        {
-            /*Verificar se o caracter está na senha e na posição correta*/
-            if (senha_inserida[k] == senha[k])
-            {  
-                printf("%d esta na posicao correta\n",senha_inserida[k]);
-                pos_certa += 1;
-            }
-            /*Verifica se o caracter está na senha, porém na posição incorreta*/
-            else if((senha_inserida[k] == senha[0]) ||
-                    (senha_inserida[k] == senha[1]) ||
-                    (senha_inserida[k] == senha[2]) ||
-                    (senha_inserida[k] == senha[3]))
-            {
-                printf("%d esta na posicao incorreta, mas esta na senha\n",senha_inserida[k]);
-                pos_errada += 1;
-            }
-            /*Caso onde o usuário não acertou nenhum caracter da senha, mesmo fora de ordem*/
-            else if((k == 3) && (pos_certa == 0) && (pos_errada == 0))
-            {
-                printf("Nenhum dos caracteres inseridos esta na senha\n");
-            }
-            /*Caso onde todos os caracteres foram inseridos na posição correta*/
-            if (pos_certa == 4)
-            {
-                printf("Parabens voce acertou a senha!");
-                return 1;
-            }
-        } 
-        pos_certa = 0;
-        pos_errada = 0;
-        tentativa++;
-    } 
+                  
+                
+               
+               printf("Fim de jogo! \n\n");
+        
+    
     return 0;
 }
+
+
+
+
+
+
